507e566e989a89f21e4a0d31eb407d95aff6e69d
jeffreyHoelzel <jmh2338@nau.edu>
2024-12-26 10:50:43 -0700

    Added icons

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const {app, BrowserWindow} = require("electron");
+const path = require("path");
 require("electron-reload")(__dirname);
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800, 
-    height: 600
+    height: 600, 
+    webPreferences: {
+      nodeIntegration: true
+    }, 
+    icon: path.join(__dirname, "src/icons/favicon.ico")
   });
 
   mainWindow.loadFile("./src/index.html");
